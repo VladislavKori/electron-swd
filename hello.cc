06c994d69be979a9b6806413907243e418b95e29
VladislavKori <vladislav.kori.work@gmail.com>
2024-01-14 15:27:45 +0500

    v2

--- hello.cc
+++ hello.cc
@@ -1,36 +1,84 @@
 // hello.cc
-#include <node.h>
+#include <node_api.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
 
-using namespace v8;
+using namespace std;
 
-void Method(const FunctionCallbackInfo<Value> &args)
+void PrintErrorMessage(DWORD dwErr)
 {
-  Isolate *isolate = Isolate::GetCurrent();
-  HandleScope scope(isolate);
+  setlocale(LC_ALL, "Russian");
+  LPVOID lpMsgBuf;
 
-  if (args.Length() < 2)
-  {
-    isolate->ThrowException(Exception::TypeError(
-        String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
-    return;
-  }
+  FormatMessage(
+      FORMAT_MESSAGE_ALLOCATE_BUFFER |
+          FORMAT_MESSAGE_FROM_SYSTEM |
+          FORMAT_MESSAGE_IGNORE_INSERTS,
+      NULL,
+      dwErr,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      (LPTSTR)&lpMsgBuf,
+      0, NULL);
 
-  if (!args[0]->IsNumber() || !args[1]->IsNumber())
-  {
-    isolate->ThrowException(Exception::TypeError(
-        String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
-    return;
-  }
+  printf("Error code: %d\n", dwErr);
+  printf("Error message: %s\n", (char *)lpMsgBuf);
 
-  double value = args[0]->NumberValue(isolate->GetCurrentContext()).FromJust() + args[1]->NumberValue(isolate->GetCurrentContext()).FromJust();
-  Local<Number> num = Number::New(isolate, value);
-
-  args.GetReturnValue().Set(num);
+  LocalFree(lpMsgBuf);
 }
 
-void Initialize(Local<Object> exports)
+napi_value MyFunc(napi_env env, napi_callback_info info)
 {
-  NODE_SET_METHOD(exports, "myCusttomFunc", Method);
-};
+  size_t argc = 1;
+  napi_value argv[1];
+  bool ok = true;
+  napi_value result;
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+  char16_t buffer[100];
+  size_t value;
+
+  napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+  napi_get_value_string_utf16(env, argv[0], buffer, sizeof(buffer) / sizeof(buffer[0]), &value);
+
+  std::u16string utf16str(buffer, value);
+  std::string str(utf16str.begin(), utf16str.end());
+
+  cout << str << endl;
+  unsigned long num = std::stoul(str, nullptr, 16);
+  HWND hwnd = reinterpret_cast<HWND>(num);
+
+  DWORD hel = SetWindowPos(hwnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+  if (hel == 0)
+
+  {
+    ok = false;
+    DWORD errorCode = GetLastError();
+    PrintErrorMessage(errorCode);
+  }
+  else
+  {
+    ok = true;
+  }
+  napi_get_boolean(env, ok, &result);
+  return result;
+}
+
+napi_value init(napi_env env, napi_value exports)
+{
+  napi_status status;
+
+  napi_value fn;
+
+  status = napi_create_function(env, nullptr, 0, MyFunc, nullptr, &fn);
+  if (status != napi_ok)
+    return NULL;
+
+  status = napi_set_named_property(env, exports, "sayHello", fn);
+  if (status != napi_ok)
+    return NULL;
+
+  return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const hello = require("./build/Release/hello");
+const { BrowserWindow, app, ipcMain } = require("electron");
 
-console.log(hello.myCusttomFunc(10, 20));
+// console.log(hello.sayHello("sdfg"));
+let win;
+
+const createWindow = () => {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+ipcMain.on("to-bottom", (_) => {
+  const hbuf = win.getNativeWindowHandle().readUInt32LE().toString(16);
+  console.log(hbuf);
+
+  hello.sayHello(hbuf);
+});
+
+app.whenReady().then(() => {
+  createWindow();
+});
